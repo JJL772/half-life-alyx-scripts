a00a38141f37c903fe423045774074649318b8f3
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-05-23 21:55:44 -0700

    Improved & fixed the jumpfix command

--- vscripts/user/jumpfix.lua
+++ vscripts/user/jumpfix.lua
@@ -1,12 +1,16 @@
-Convars:RegisterCommand("jumpfixed", function()
-    local player = Entities:GetLocalPlayer()
-    if player:GetVelocity().z == 0 then 
-        player:ApplyAbsVelocityImpulse    (Vector(0,0,300))
-        player:SetThink(normalizeJump, self, .02)
-    end
-end, nil, 0)
+-- Fix for jumping. Thanks to jflowbflow for the normalizeJump stuff!
 
-function normalizeJump()
-    local vel = player:GetVelocity() 
-    player:SetVelocity(Vector(vel.x, vel.y, 130))
+Convars:RegisterConvar("sv_jump_force", "150", "The force applied to the player when jumping", 0)
+
+Convars:RegisterCommand("jumpfixed", function()
+	local player = Entities:GetLocalPlayer()
+	if player:GetVelocity().z == 0 then 
+		player:ApplyAbsVelocityImpulse(Vector(0,0,300))
+		player:SetThink(normalizeJump, self, 0.02)
+	end
+end, "Jump, but fixed!", 0)
+
+function normalizeJump(player)
+	local vel = player:GetVelocity()
+	player:SetVelocity(Vector(vel.x, vel.y, Convars:GetFloat("sv_jump_force")))
 end
